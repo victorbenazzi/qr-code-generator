d228f7cd167630f373116f093c4d31e7dd9d33f8
Victor <victormatosbenazzi@gmail.com>
2024-02-14 00:40:05 -0300

    Atualiza a posição do ToastContainer para "top-right"

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ export default function App() {
   return (
     <>
     <ToastContainer 
-   position="bottom-right"
+   position="top-right"
    autoClose={5000}
    hideProgressBar={false}
    newestOnTop={false}
